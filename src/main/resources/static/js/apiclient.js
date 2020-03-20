1e89517155d844090491ece09585bf120e8cba49
CarlosGomez380 <53835467+CarlosGomez380@users.noreply.github.com>
2020-03-20 16:31:12 -0500

    URL heroku

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -3,7 +3,7 @@ var apiclient=(function(){
     function getAirportsByName(){
         axios({
             method:'get',
-            url: "http://carlos-gomez-arsw-t2.herokuapp.com/airports/" + $("#airportName").val(),
+            url: "/airports/" + $("#airportName").val(),
 
         })
         .then(response => app.createTable(response.data))
